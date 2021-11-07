3ca77ff709591174b9791cc8f876f2074be44ebd
mihirkumar1212 <pmihir2609@gmail.com>
2021-11-07 11:26:27 -0500

    Database Created

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # iMarket-Student-Edition
 Mobile App written in Java for students who wish to sell products online
+
+## Group Members/Collaborators:
+
+1. Esam Uddin - 100711116
+2. Tegveer Singh - 100730432
+3. Mihir Patel - 100702168
+4. Clarissa Branje - 100716458
+5. Ashwin Shanmugam - 100700236
---
+++ app/src/main/java/com/example/imarket_student_edition/MyDatabase.java
@@ -0,0 +1,72 @@
+package com.example.imarket_student_edition;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+public class MyDatabase  extends SQLiteOpenHelper {
+
+    private Context context;
+    // naming the database
+    private static final String DATABASE_NAME = "iMarket.db";
+    private static final int DATABASE_VERSION = 1;
+    // table 1
+    private static final String User_Table = "User";
+    private static final String User_Column_ID = "ID";
+    private static final String User_Column_Name = "Name";
+    private static final String User_Column_Email = "Email";
+    private static final String User_Column_Password = "Password";
+    private static final String User_Column_DOB = "DOB";
+    private static final String User_Column_Location = "Location";
+    private static final String User_Column_DateCreated = "DateCreated";
+
+    // table 2
+    private static final String Product_Table = "Product";
+    private static final String Product_Column_ID = "ID";
+    private static final String Product_Column_Name = "Name";
+    private static final String Product_Column_Image_video = "imgVideo";
+    private static final String Product_Column_Description = "Description";
+    private static final String Product_Column_DateAdded = "DateAdded";
+    private static final String Product_Column_Price = "Price";
+    private static final String Product_Column_UserID = "UserId";
+
+
+    public MyDatabase(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        this.context = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String query = "CREATE TABLE " + User_Table +
+                " (" + User_Column_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                User_Column_Name + " TEXT," +
+                User_Column_Email  + " TEXT," +
+                User_Column_Password + " PASSWORD," +
+                User_Column_DOB + " DATE," +
+                User_Column_Password + " Password," +
+                User_Column_Location + " Text," +
+                User_Column_DateCreated + " DATE);";
+
+        db.execSQL(query);
+
+        String query2= "CREATE TABLE " + Product_Table +
+                " (" + Product_Column_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                Product_Column_Name + " TEXT," +
+                Product_Column_Image_video  + " TEXT," +
+                Product_Column_Description + " TEXT," +
+                Product_Column_DateAdded + " DATE," +
+                Product_Column_Price + " DOUBLE," +
+                Product_Column_UserID + " INTEGER);";
+
+        db.execSQL(query2);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + User_Table);
+        db.execSQL("DROP TABLE IF EXISTS " + Product_Table);
+        onCreate(db);
+    }
+}
