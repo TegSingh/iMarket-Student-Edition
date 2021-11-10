914f14b7a420903ee0fad7a24fb4dc9181baf5fc
esam191 <esam191@hotmail.com>
2021-11-09 22:36:22 -0500

    resolving conflict

--- app/src/main/java/com/example/imarket_student_edition/Activities/UserAuthenticationActivity.java
+++ app/src/main/java/com/example/imarket_student_edition/Activities/UserAuthenticationActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
+import com.example.imarket_student_edition.DatabaseHelper.MyDatabase;
 import com.example.imarket_student_edition.R;
 
 public class UserAuthenticationActivity extends AppCompatActivity {
@@ -19,6 +21,24 @@ public class UserAuthenticationActivity extends AppCompatActivity {
     // Method to Log the user in
     public void loginButtonClickListener(View v) {
         System.out.println("Login button was clicked");
+
+        EditText editTextEmail = findViewById(R.id.editTextEmail);
+        EditText editTextPassword = findViewById(R.id.editTextPassword);
+        String email = editTextEmail.getText().toString();
+        String password = editTextPassword.getText().toString();
+        System.out.println("Signing in: Email: " + email);
+
+        // Get DataBase helper for checking user
+        MyDatabase database_helper = new MyDatabase(this);
+        boolean result = database_helper.authenticateUser(email, password);
+        if (result) {
+            System.out.println("User authenticated successfully");
+            // ADD ANY REQUIRED INTENTS OR LINKS HERE
+        } else {
+            System.out.println("Couldnt find user information");
+            // ADD ANY REQUIRED INTENTS OR ERROR MESSAGES HERE
+        }
+
         Intent i = new Intent(this, ProductPageActivity.class);
         startActivity(i);
     }
