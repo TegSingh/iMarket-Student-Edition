6fb6b625bba5119cc868cb4442eaaf017ec104c6
mihirkumar1212 <pmihir2609@gmail.com>
2021-11-09 23:16:02 -0500

    Added navbar

--- app/src/main/java/com/example/imarket_student_edition/Activities/MainActivity.java
+++ app/src/main/java/com/example/imarket_student_edition/Activities/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.imarket_student_edition.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 
 import com.example.imarket_student_edition.R;
@@ -14,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     // Button event listener to start the application
@@ -22,5 +27,12 @@ public class MainActivity extends AppCompatActivity {
         // Create intent to move to login activity and start activity
         Intent start_application_intent = new Intent(this, UserAuthenticationActivity.class);
         startActivity(start_application_intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
     }
 }
